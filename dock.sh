4988c013f61733a05f4be133e01494f295a2b9ec
Christian James <c.james.aus@gmail.com>
2019-05-09 01:03:28 +1000

    Moved app into client directory and started a container network

--- dock.sh
+++ dock.sh
@@ -15,13 +15,13 @@ FOLDER_NAME="$(rev $FOLDER_NAME)"
 FOLDER_NAME="$(cut -d'/' -f1 <<<$FOLDER_NAME)"
 FOLDER_NAME="$(rev $FOLDER_NAME)"
 
-# DOCKER_IMAGE="$(docker images -f=reference=$FOLDER_NAME'*node*' -q)"
+# DOCKER_IMAGE="$(docker images -f=reference=$FOLDER_NAME'*client*' -q)"
 # DOCKER_CONTAINER="$(docker ps -f ancestor=$DOCKER_IMAGE)"
 # DOCKER_CONTAINER="$(echo "$DOCKER_CONTAINER" | grep -oE "("$FOLDER_NAME"_.*^[[:blank:]]*_[[:digit:]])$")"
 # echo $DOCKER_IMAGE
 # echo $DOCKER_CONTAINER
 
-DOCKER_SERVICE="node"
+DOCKER_SERVICE="client"
 DOCKER_CONTAINER="${FOLDER_NAME}_${DOCKER_SERVICE}_1"
 
 DOCKER_CMD="echo 'Enter a command to run...'"
@@ -32,6 +32,9 @@ if [[ "$1" = "up" ]] ; then
 elif [[ "$1" = "stop" ]] ; then
   DOCKER_CMD="docker stop $DOCKER_CONTAINER"
   echo "Stopping container '$DOCKER_CONTAINER'..."
+elif [[ "$1" = "rm" ]] ; then
+  DOCKER_CMD="docker rm $DOCKER_CONTAINER"
+  echo "Removing container '$DOCKER_CONTAINER'..."
 elif [[ "$1" = "yarn" ]] ; then
   DOCKER_CMD="winpty docker exec -it $DOCKER_CONTAINER ${@}"
   echo "Running ${@} in container '$DOCKER_CONTAINER'..." 
